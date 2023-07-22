83c79fa2a07a48fd91b8255247e412822eecb3f7
fcrouchet <fcrouchet@gmail.com>
2023-07-22 19:52:48 +0300

    Petits changements

--- streamlit_app/tabs/third_tab.py
+++ streamlit_app/tabs/third_tab.py
@@ -15,9 +15,15 @@ celles proposées par les auteurs du dataset, ainsi que sur celles trouvées
 dans d’autres articles scientifiques traitant de problématiques similaires.
 """
 
-header1 = "Filtres numériques"
-subheader11 = "Filtre passe-bande (passe-bas + passe-haut)"
-text11= """Pour les données EEG, un filtre passe-haut de 0.01 HZ et un filtre
+header_filtres = "Filtres numériques"
+subheader_filtres = "Filtre passe-bande (passe-bas + passe-haut)"
+txt_filtres = """Le filtre passe-haut laisse passer les hautes fréquences
+et coupe les basses fréquences, par rapport à une fréquence de référence (fc
+sur le schéma), et inversement pour le filtre basse-bas ; Tout simplement en
+combinant les deux, on peut obtenir un filtre passe-bande ou bien un filtre
+coupe-bande."""
+img_filtres = "../images/ExplicationFiltres.png"
+text_filtreseeg= """Pour les données EEG, un filtre passe-haut de 0.01 HZ et un filtre
 passe-bas de 24.49 Hz ont été appliqués. Ce sont des filtres utilisés de
 manière conventionnelle dans l’analyse de signal EEG et qui permettent de
 conserver les signaux physiologiques tout en retirant des bruits parasites
@@ -26,8 +32,6 @@ comme le 60Hz (courant du casque) par exemple."""
 rawo = mne.io.read_raw_fif("../data/S1_eeg.fif",preload=True)
 rawo10 = rawo.copy()
 rawo10.crop(tmax=10).load_data()
-rawo50 = rawo.copy()
-rawo50.crop(tmax=50).load_data()
 
 def _update_slider(value):
     st.session_state["filter_slider"] = value
@@ -57,15 +61,16 @@ def display_ica():
         img_ica="../images/EEGApresFiltrage.png"
     return img_ica
 
-subheader12 = "Filtre Butterworth"
-text12= """Pour les données fNIRS, nous avons utilisé un filtre passe-bande
+subheader_butterworth = "Filtre Butterworth"
+text_butterworth= """Pour les données fNIRS, nous avons utilisé un filtre passe-bande
 particulier, un filtre Butterworth d'ordre 4. Le gain de ce filtre est le
 plus constant possible dans la bande passante et tend vers 0 dB dans la bande
 de coupure.
 """
+img_butterworth = "../images/Butterworth_orders.png"
 
-header2 = "Independant Components Analysis sur signal EEG"
-text2 = """L'analyse en composantes indépendantes ou ICA permet d’identifier
+header_ICA = "Independant Components Analysis sur les signaux EEG"
+text_ICA = """L'analyse en composantes indépendantes ou ICA permet d’identifier
 et supprimer les artefacts pouvant provenir de diverses sources, telles
 que les mouvements oculaires, les battements du coeur, les bruits électriques,
 etc... L'ICA permet de séparer ces sources artefactuelles des signaux
@@ -73,13 +78,14 @@ cérébraux d'intérêt, facilitant ainsi une meilleure interprétation et
 analyse des données.
 """
 
-header3 = "Concentration en HbO et HbR sur signaux fNIRS"
-text3 = """Les deux signaux utilisés en fNIRS correspondant à deux longueurs
+header_concentration = "Concentration en HbO et HbR sur les signaux fNIRS"
+text_concentration = """Les deux signaux utilisés en fNIRS correspondant à deux longueurs
 d’onde (760 nm et 850 nm) ont également subi une transformation mathématique
 pour les transformer en signaux de concentration d’hémoglobine oxygénée (HbO)
 et d’hémoglobine désoxygénée (HbR). Cette transformation a été réalisée
 en appliquant la loi de Beer-Lambert.
 """
+img_absorption="../images/AbsorptionHbOHbR.png"
 hbo_righthand_FC1_1="../images/NIRS_hbo_righthand_FC1_1.png"
 hbo_righthand_FC1_2="../images/NIRS_hbo_righthand_FC1_2.png"
 hbo_righthand_FC1_3="../images/NIRS_hbo_righthand_FC1_3.png"
@@ -87,18 +93,27 @@ hbr_righthand_FC1_1="../images/NIRS_hbr_righthand_FC1_1.png"
 hbr_righthand_FC1_2="../images/NIRS_hbr_righthand_FC1_2.png"
 hbr_righthand_FC1_3="../images/NIRS_hbr_righthand_FC1_3.png"
 
-def run():
 
-    st.write(" ")
-    st.write(" ")
-    st.write(" ")
+def run():
 
     st.title(title)
     st.write(text)
 
-    st.header(header1)
-    st.subheader(subheader11)
-    st.write(text11)
+    st.header(header_filtres)
+    st.subheader(subheader_filtres)
+    col1, col2 = st.columns(2)
+    with col1:
+        st.write(txt_filtres)
+    with col2:
+        st.image(img_filtres)
+    st.write(text_filtreseeg)
+    st.subheader(subheader_butterworth)    
+    col1, col2 = st.columns([3, 2])
+    with col1:
+        st.image(img_butterworth)
+    with col2:
+        st.write(text_butterworth)
+    st.subheader("Exemple sur données EEG")
     col1, col2 = st.columns([4, 1])
     with col1:
         filtre=st.slider("Filtre",min,max,value=(ph,pb),step=step,key="filter_slider",on_change=display_fig)
@@ -107,19 +122,20 @@ def run():
         st.checkbox("Butterworth Ordre 4", on_change=display_fig, key="check_butter")
     st.pyplot(display_fig())
 
-    st.subheader(subheader12)
-    st.write(text12)
-    
-    st.header(header2)
-    st.write(text2)
+    st.header(header_ICA)
+    st.write(text_ICA)
     col1, col2 = st.columns([1, 3])
     with col1:
         scenario = st.radio("Scenario:",("Pas de filtre et d'ICA","Avec filtre et ICA 4 composants principaux"),key="radio_ica",on_change=display_ica)
     with col2:
         st.image(display_ica())
     
-    st.header(header3)
-    st.write(text3)
+    st.header(header_concentration)
+    col1, col2 = st.columns([1, 2])
+    with col1:
+        st.write(text_concentration)
+    with col2:
+        st.image(img_absorption)
     st.write("1) Signaux originaux **sans transformation** pour les longueurs d’ondes 760 et 850 nm")
     col1, col2 = st.columns(2)
     with col1:
