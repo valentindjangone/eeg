22e381f770705be17b22ddb6c0c3d65b71986720
fcrouchet <fcrouchet@gmail.com>
2023-07-22 20:22:54 +0300

    Petits changements

--- streamlit_app/tabs/third_tab.py
+++ streamlit_app/tabs/third_tab.py
@@ -6,7 +6,7 @@ sidebar_name = "Preprocessing"
 
 title = "Méthodes de Preprocessing utilisées"
 text = """Le pretraitemement des données, ou preprocessing, est
-indispensable à l'analyse des signaux EEG et NIRS. Il permet notament
+indispensable à l'analyse des signaux EEG et NIRS. Il permet notamment
 de s'étalonner par rapport aux électrodes de référence, de réduire les
 bruits parasites, de supprimer les mauvais canaux, ou encore d'éliminer les
 artéfacts de mouvement.
@@ -29,6 +29,15 @@ manière conventionnelle dans l’analyse de signal EEG et qui permettent de
 conserver les signaux physiologiques tout en retirant des bruits parasites
 comme le 60Hz (courant du casque) par exemple."""
 
+subheader_butterworth = "Filtre Butterworth"
+text_butterworth= """Pour les données fNIRS, nous avons utilisé un filtre passe-bande
+particulier, un filtre Butterworth d'ordre 4. Le gain de ce filtre est le
+plus constant possible dans la bande passante et tend vers 0 dB dans la bande
+de coupure. La fréquence pour le filtre passe-haut était de 0,01Hz et 0,2Hz pour
+le filtre passe-bas.
+"""
+img_butterworth = "../images/Butterworth_orders.png"
+
 rawo = mne.io.read_raw_fif("../data/S1_eeg.fif",preload=True)
 rawo10 = rawo.copy()
 rawo10.crop(tmax=10).load_data()
@@ -39,18 +48,19 @@ def _update_slider(value):
 if "filter_slider" not in st.session_state:
     st.session_state["filter_slider"] = 0
 ph, pb =0.01, 24.49
-min, max = 0.0, 100.0
+min, max = 0.0, 80.0
 step = 0.01
 
 def display_fig():
     pht,pbt=st.session_state["filter_slider"]
+    canal=st.session_state["select_canal"]
     raw=rawo10.copy()
     if st.session_state["check_butter"]:
         raw.filter(pht,pbt,method='iir', iir_params = None)
     else:
         raw.filter(pht,pbt)
     fig, ax = plt.subplots(figsize=(24, 12))
-    ax.plot(raw.times, raw.get_data(picks='FC1').squeeze())
+    ax.plot(raw.times, raw.get_data(picks=canal).squeeze())
     return fig
 
 def display_ica():
@@ -60,14 +70,6 @@ def display_ica():
     else:
         img_ica="../images/EEGApresFiltrage.png"
     return img_ica
-
-subheader_butterworth = "Filtre Butterworth"
-text_butterworth= """Pour les données fNIRS, nous avons utilisé un filtre passe-bande
-particulier, un filtre Butterworth d'ordre 4. Le gain de ce filtre est le
-plus constant possible dans la bande passante et tend vers 0 dB dans la bande
-de coupure.
-"""
-img_butterworth = "../images/Butterworth_orders.png"
 
 header_ICA = "Independant Components Analysis sur les signaux EEG"
 text_ICA = """L'analyse en composantes indépendantes ou ICA permet d’identifier
@@ -120,6 +122,7 @@ def run():
     with col2:
         st.button("Defaut : 0,01 à 24,49", on_click=_update_slider, kwargs={"value": (ph,pb)})
         st.checkbox("Butterworth Ordre 4", on_change=display_fig, key="check_butter")
+    st.selectbox("Choix du canal :", options=("FC1", "FC2","FC5","FC6","Fz"), index=0, on_change=display_fig, key="select_canal")
     st.pyplot(display_fig())
 
     st.header(header_ICA)
